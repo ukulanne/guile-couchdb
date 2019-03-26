7c1a96b4d9bc458cffa394d4b91962048c6e8022
Anne Summers <ukulanne@gmail.com>
2019-03-26 18:14:39 -0500

    Update couchdb.scm

--- couchdb.scm
+++ couchdb.scm
@@ -2,10 +2,10 @@
 ;; Anne Summers                  ;;
 ;; <ukulanne@gmail.com>          ;;
 ;; March 25, 2019                ;;
-;; Guile couchdb wrapper         ;;
+;; Guile web app server example  ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;    This program is free software: you can redistribute it and/or modify         ;;
 ;;    it under the terms of the GNU Lesser General Public License as published by  ;;
 ;;    the Free Software Foundation, either version 3 of the License, or            ;;
@@ -22,7 +22,8 @@
 
 ;; Time-stamp: <2019-03-08 14:55:40 panda> 
 
-(use-modules (web uri)
+(use-modules (rnrs bytevectors) 
+             (web uri)
 	     (web client))
 
 (define COUCHDB-SERVER "localhost")
@@ -31,7 +32,7 @@
 (define COUCHDB-PASSWORD #f)
 (define COUCHDB-DB #f)
 
-;; (couchdb-use args) ;; server user password
+;; (couchdb-use db)
 ;; (couchdb-create db)
 ;; (couchdb-get cdb id)
 ;; (couchdb-insert cdb id . rev)
@@ -45,21 +46,18 @@
     (display (string-append COUCHDB-SERVER "5984" "/" db))
     (call-with-values
         (lambda ()  (http-put uri #:keep-alive? #f))
-      (lambda (request body) body))))
+      (lambda (request body) (utf8->string body)))))
 
 (define (couchdb-list cdb)
   (let ((uri (build-uri 'http #:host COUCHDB-SERVER #:port COUCHDB-PORT
                         #:path (string-append "/" cdb "?include_docs=true"))))
     (call-with-values
         (lambda ()  (http-get uri #:keep-alive? #f))
-      (lambda (request body) body))))
+      (lambda (request body) (utf8->string body)))))
 
 (define (couchdb-get cdb id)
   (let ((uri (build-uri 'http #:host COUCHDB-SERVER #:port COUCHDB-PORT
                         #:path (string-append "/" cdb "/" id "?include_docs=true"))))
     (call-with-values
-        (lambda ()  (http-get uri #:keep-alive? #f))
-      (lambda (request body) body))))
-
-(display (couchdb-create "panda"))
-(display (couchdb-get "panda" "tohui"))
+        (lambda ()  (http-get uri #:decode-body? #t #:keep-alive? #f))
+      (lambda (request body) (utf8->string body)))))
