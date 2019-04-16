d880730bca8a17a2e0746f79d6072089b3f2907a
Anne Summers <ukulanne@gmail.com>
2019-04-15 21:49:56 -0500

    couchdb-fav-ico

--- couchdb.scm
+++ couchdb.scm
@@ -5,7 +5,7 @@
 ;; Couchdb guile wrapper         ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Time-stamp: <2019-04-13 23:13:49 panda> 
+;; Time-stamp: <2019-04-15 21:49:37 panda> 
 
 ;; Copyright (C) 2019 Anne Summers <ukulanne@gmail.com>
 
@@ -29,7 +29,7 @@
   #:use-module (web uri)
   #:use-module (web client)
   #:export (couchdb-active-tasks couchdb-db-bulk-get couchdb-db-create couchdb-db-changes couchdb-db-list couchdb-db-all-docs couchdb-doc-delete
-            couchdb-doc-get couchdb-doc-insert  couchdb-db-insert-bulk couchdb-doc-list couchdb-membership couchdb-root couchdb-server-info
+            couchdb-doc-get couchdb-doc-insert  couchdb-db-insert-bulk couchdb-doc-list couchdb-fav-ico couchdb-membership couchdb-root couchdb-server-info
             couchdb-server! couchdb-up? couchdb-version couchdb-uuids))
 
 (define CDB-PROTOCOL 'http)
@@ -55,6 +55,7 @@
       (call/wv (lambda () (,verb uri #:keep-alive? #f #:body json #:headers `((content-type . (application/json)))))
                (lambda (request body) (utf8->string body))))))
 
+;;(define (couchdb-server-from-env))
 (define (couchdb-server-info) (string-append (symbol->string CDB-PROTOCOL) "://" CDB-SERVER ":" (number->string CDB-PORT)))
 (define (couchdb-server! protocol url port)
   (set! CDB-PROTOCOL protocol)
@@ -66,12 +67,13 @@
 
 (define-couchdb-api couchdb-root           http-get  "/" "/")
 (define-couchdb-api couchdb-active-tasks   http-get  "/_active_tasks" "")
+(define-couchdb-api couchdb-fav-ico        http-get  "/favicon.ico" "")
 (define-couchdb-api couchdb-membership     http-get  "_membership" "")
 (define-couchdb-api couchdb-up?            http-get  "/_up" "/")
 (define-couchdb-api couchdb-uuids          http-get  "/_uuids?count=" "")
 (define-couchdb-api couchdb-version        http-get  "" "")
 (define-couchdb-api couchdb-db-changes     http-get  "/" " /db/_changes?style=all_docs")
-(define-couchdb-api couchdb-db-list        http-get  "/_all_dbs" "")
+(define-couchdb-api couchdb-db-list        http-get  "/_all_dbs" "") 
 (define-couchdb-api couchdb-db-create      http-put  "/" "")
 (define-couchdb-api couchdb-db-all-docs    http-get  "/" "/_all_docs")
 
